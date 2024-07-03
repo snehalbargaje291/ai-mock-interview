2e1f0e2f0a289818917180d93f84dba486733513
snehalbargaje291 <snehal.bargaje291@gmail.com>
2024-07-03 22:42:07 +0530

    faq

--- components/ui/faq.jsx
+++ components/ui/faq.jsx
@@ -1,6 +1,12 @@
 "use client";
 import React, { useState, useRef } from "react";
 import { motion, useInView } from "framer-motion";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import { ChevronsUpDown } from "lucide-react";
 
 const Accordion = () => {
   const ref = useRef(null);
@@ -68,11 +74,25 @@ const Accordion = () => {
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4 grid grid-cols-1 lg:grid-cols-2 gap-4">
             {questions.map((item, index) => (
-              <AccordionItem
-                key={index}
-                header={item.header}
-                text={item.text}
-              />
+              // <AccordionItem
+              //   key={index}
+              //   header={item.header}
+              //   text={item.text}
+              // />
+              <Collapsible
+                      key={index}
+                      className="bg-gradient-to-r from-gray-200 via-gray-100 to-gray-50 p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
+                    >
+                      <CollapsibleTrigger className="w-full text-sm font-bold text-left flex justify-between items-center">
+                        <span className="text-gray-800">{item.header}</span>
+                        <ChevronsUpDown className="w-6 h-6 text-purple-500" />
+                      </CollapsibleTrigger>
+                      <CollapsibleContent className="mt-4 space-y-4 text-sm">
+                        <p className="text-gray-500 text-xs">
+                        {item.text}
+                        </p>
+                      </CollapsibleContent>
+                    </Collapsible>
             ))}
           </div>
         </div>
@@ -112,64 +132,64 @@ const Accordion = () => {
 
 export default Accordion;
 
-const AccordionItem = ({ header, text }) => {
-  const [active, setActive] = useState(false);
-  const ref = useRef(null);
-  const inView = useInView(ref, { once: false });
+// const AccordionItem = ({ header, text }) => {
+//   const [active, setActive] = useState(false);
+//   const ref = useRef(null);
+//   const inView = useInView(ref, { once: false });
 
-  const handleToggle = (e) => {
-    e.preventDefault();
-    setActive(!active);
-  };
+//   const handleToggle = (e) => {
+//     e.preventDefault();
+//     setActive(!active);
+//   };
 
-  return (
-    <motion.div
-      ref={ref}
-      initial={{ opacity: 0, y: 50, scale: 0.95 }}
-      animate={
-        inView
-          ? { opacity: 1, y: 0, scale: 1 }
-          : { opacity: 0, y: 50, scale: 0.95 }
-      }
-      transition={{ duration: 0.6 }}
-      className="mb-6 w-full rounded-lg bg-white p-4 shadow-[0px_20px_95px_0px_rgba(201,203,204,0.30)]"
-    >
-      <button
-        className={`faq-btn flex w-full text-left`}
-        onClick={handleToggle}
-      >
-        <div className="mr-5 flex h-6 w-full max-w-[40px] items-center justify-center rounded-lg text-primary dark:bg-white/5">
-          <svg
-            className={`fill-primary stroke-primary duration-200 ease-in-out ${
-              active ? "rotate-180" : ""
-            }`}
-            width="17"
-            height="10"
-            viewBox="0 0 17 10"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M7.28687 8.43257L7.28679 8.43265L7.29496 8.43985C7.62576 8.73124 8.02464 8.86001 8.41472 8.86001C8.83092 8.86001 9.22376 8.69083 9.53447 8.41713L9.53454 8.41721L9.54184 8.41052L15.7631 2.70784L15.7691 2.70231L15.7749 2.69659C16.0981 2.38028 16.1985 1.80579 15.7981 1.41393C15.4803 1.1028 14.9167 1.00854 14.5249 1.38489L8.41472 7.00806L2.29995 1.38063L2.29151 1.37286L2.28271 1.36548C1.93092 1.07036 1.38469 1.06804 1.03129 1.41393L1.01755 1.42738L1.00488 1.44184C0.69687 1.79355 0.695778 2.34549 1.0545 2.69659L1.05999 2.70196L1.06565 2.70717L7.28687 8.43257Z"
-              fill=""
-              stroke=""
-            />
-          </svg>
-        </div>
-        <div className="w-full">
-          <h4 className="mt-1 text-sm font-semibold text-dark dark:text-white">
-            {header}
-          </h4>
-        </div>
-      </button>
-      <div
-        className={`pl-[62px] duration-200 ease-in-out ${
-          active ? "block" : "hidden"
-        }`}
-      >
-        <p className="py-3 text-xs leading-relaxed text-body-color dark:text-dark-6">
-          {text}
-        </p>
-      </div>
-    </motion.div>
-  );
-};
+//   return (
+//     <motion.div
+//       ref={ref}
+//       initial={{ opacity: 0, y: 50, scale: 0.95 }}
+//       animate={
+//         inView
+//           ? { opacity: 1, y: 0, scale: 1 }
+//           : { opacity: 0, y: 50, scale: 0.95 }
+//       }
+//       transition={{ duration: 0.6 }}
+//       className="mb-6 w-full rounded-lg bg-white p-4 shadow-[0px_20px_95px_0px_rgba(201,203,204,0.30)]"
+//     >
+//       <button
+//         className={`faq-btn flex w-full text-left`}
+//         onClick={handleToggle}
+//       >
+//         <div className="mr-5 flex h-6 w-full max-w-[40px] items-center justify-center rounded-lg text-primary dark:bg-white/5">
+//           <svg
+//             className={`fill-primary stroke-primary duration-200 ease-in-out ${
+//               active ? "rotate-180" : ""
+//             }`}
+//             width="17"
+//             height="10"
+//             viewBox="0 0 17 10"
+//             xmlns="http://www.w3.org/2000/svg"
+//           >
+//             <path
+//               d="M7.28687 8.43257L7.28679 8.43265L7.29496 8.43985C7.62576 8.73124 8.02464 8.86001 8.41472 8.86001C8.83092 8.86001 9.22376 8.69083 9.53447 8.41713L9.53454 8.41721L9.54184 8.41052L15.7631 2.70784L15.7691 2.70231L15.7749 2.69659C16.0981 2.38028 16.1985 1.80579 15.7981 1.41393C15.4803 1.1028 14.9167 1.00854 14.5249 1.38489L8.41472 7.00806L2.29995 1.38063L2.29151 1.37286L2.28271 1.36548C1.93092 1.07036 1.38469 1.06804 1.03129 1.41393L1.01755 1.42738L1.00488 1.44184C0.69687 1.79355 0.695778 2.34549 1.0545 2.69659L1.05999 2.70196L1.06565 2.70717L7.28687 8.43257Z"
+//               fill=""
+//               stroke=""
+//             />
+//           </svg>
+//         </div>
+//         <div className="w-full">
+//           <h4 className="mt-1 text-sm font-semibold text-dark dark:text-white">
+//             {header}
+//           </h4>
+//         </div>
+//       </button>
+//       <div
+//         className={`pl-[62px] duration-200 ease-in-out ${
+//           active ? "block" : "hidden"
+//         }`}
+//       >
+//         <p className="py-3 text-xs leading-relaxed text-body-color dark:text-dark-6">
+//           {text}
+//         </p>
+//       </div>
+//     </motion.div>
+//   );
+// };
